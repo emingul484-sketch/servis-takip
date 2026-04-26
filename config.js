0117b658652fcd189c867bb8c0c202401857fcb4
emingul484-sketch <emingul484@gmail.com>
2026-04-27 01:01:45 +0300

    config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyDn179sk4QyEh7TYuoU1KXcaXtigqWus6I",
+    authDomain: "servis-56e5d.firebaseapp.com",
+    projectId: "servis-56e5d",
+    storageBucket: "servis-56e5d.firebasestorage.app",
+    messagingSenderId: "497738142821",
+    appId: "1:497738142821:web:7b23a60815048fbe21228c"
+};
+
+export const IMGBB_KEY = "0fd006aeed01a4802808abbfc44990ca";
